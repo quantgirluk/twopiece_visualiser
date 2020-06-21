8d6832524ecdaaf5f08d9667b9bff9645c4eff6f
Quant Girl <quantgirl.uk@gmail.com>
2020-06-21 23:07:27 +0100

    Updating main app.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import dash
 import dash_bootstrap_components as dbc
-
+import os
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
